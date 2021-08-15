2e0991f574a32e0678d7b7c7507344efe9bfbf83
CK <9k.chainarong@gmail.com>
2021-08-15 23:41:35 +0700

    1st

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 3001;
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views/ejs"));
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is starting at http://localhost:${PORT}`);
+});
