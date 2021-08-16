7a56f9c23ad3399eef19851cce6bbcc844b3c26d
CK <9k.chainarong@gmail.com>
2021-08-17 00:52:15 +0700

    mongoDB

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+
 const router = require("./server/routes/router");
+const connectDB = require("./server/database/connection");
 
 const app = express();
 
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 3001;
 
+app.listen(PORT, () => {
+  console.log(`server is starting at http://localhost:${PORT}`);
+});
+
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.set("view engine", "ejs");
 // app.set("views", path.resolve(__dirname, "views/ejs"));
@@ -21,7 +29,3 @@ app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.use("/", router);
-
-app.listen(PORT, () => {
-  console.log(`server is starting at http://localhost:${PORT}`);
-});
---
+++ server/database/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Database Connected ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`Error Database connection 1 : ${err}`);
+  }
+};
+
+module.exports = connectDB;
